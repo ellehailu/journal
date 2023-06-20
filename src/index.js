d5928c89cc75c614c7435d8020b1eb210d80ca89
ellehailu <hailu.elle@gmail.com>
2023-06-20 16:29:27 -0700

    EOD commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+import Journal from './journal.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+
 function handleJournalForm() {
-    event.preventDefault();
-    document.querySelector('#response').innerText = null;
-    const length2 = parseInt(document.querySelector('#length2').value);
-    const length1 = parseInt(document.querySelector('#length1').value);
-    const length3 = parseInt(document.querySelector('#length3').value);
-    const journal = new Journal(length1, length2, length3);
-    const response = journal.checkType();
-    const pTag = document.createElement("p");
-    pTag.append(response);
-    document.querySelector('#response').append(pTag);
-  }
-  
-  window.addEventListener("load", function() {
-    document.querySelector("#journal-checker-form").addEventListener("submit", handleTriangleForm);
-  });
+  event.preventDefault();
+  document.querySelector('#response').innerText = null;
+  const length2 = parseInt(document.querySelector('#length2').value);
+  const length1 = parseInt(document.querySelector('#length1').value);
+  const length3 = parseInt(document.querySelector('#length3').value);
+  const journal = new Journal(length1, length2, length3);
+  const response = journal.checkType();
+  const pTag = document.createElement("p");
+  pTag.append(response);
+  document.querySelector('#response').append(pTag);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("#journal-checker-form").addEventListener("submit", handleJournalForm);
+});
--- src/journal.js
+++ src/journal.js
@@ -1,9 +1,9 @@
-function Journal(side1, side2, side3) {
-    this.side1 = side1;
-    this.side2 = side2;
-    this.side3 = side3;
-  }
-  
-  Journal.prototype.checkType = function() {
-    return "I can't answer that yet!";
-  }
+export default function Journal(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+}
+
+Journal.prototype.checkType = function () {
+  return "I can't answer that yet!";
+};
